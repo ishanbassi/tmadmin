c33bfa092c002464c8d988b864a13d58ba340282
ishanbassi <62026183+ishanbassi@users.noreply.github.com>
2024-09-14 13:22:24 +0530

    added matching trademark dto

---
+++ src/main/java/com/bassi/tmapp/service/dto/MatchingTrademarkDto.java
@@ -0,0 +1,114 @@
+package com.bassi.tmapp.service.dto;
+
+
+
+public class MatchingTrademarkDto {
+	
+	 String matchingTrademark;
+	String registeredTrademark;
+	Integer tmClass;
+	Long applicationNo;
+	String details;
+	Integer journalNo;
+	String proprietorName;
+	String proprietorAddress;
+	String agentName;
+	String agentAddress;
+	
+
+    public MatchingTrademarkDto(String matchingTrademark, String registeredTrademark, Integer tmClass, 
+    		Long applicationNo, String details, Integer journalNo, String proprietorName, 
+                                String proprietorAddress, String agentName, String agentAddress) {
+        this.matchingTrademark = matchingTrademark;
+        this.registeredTrademark = registeredTrademark;
+        this.tmClass = tmClass;
+        this.applicationNo = applicationNo;
+        this.details = details;
+        this.journalNo = journalNo;
+        this.proprietorName = proprietorName;
+        this.proprietorAddress = proprietorAddress;
+        this.agentName = agentName;
+        this.agentAddress = agentAddress;
+    }
+	
+	public String getAgentName() {
+		return agentName;
+	}
+
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
+	}
+
+	public String getAgentAddress() {
+		return agentAddress;
+	}
+
+	public void setAgentAddress(String agentAddress) {
+		this.agentAddress = agentAddress;
+	}
+
+	public String getMatchingTrademark() {
+		return matchingTrademark;
+	}
+
+	public void setMatchingTrademark(String matchingTrademark) {
+		this.matchingTrademark = matchingTrademark;
+	}
+
+	public String getRegisteredTrademark() {
+		return registeredTrademark;
+	}
+
+	public void setRegisteredTrademark(String registeredTrademark) {
+		this.registeredTrademark = registeredTrademark;
+	}
+
+	public Integer getTmClass() {
+		return tmClass;
+	}
+
+	public void setTmClass(Integer tmClass) {
+		this.tmClass = tmClass;
+	}
+
+	public Long getApplicationNo() {
+		return applicationNo;
+	}
+
+	public void setApplicationNo(Long applicationNo) {
+		this.applicationNo = applicationNo;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public Integer getJournalNo() {
+		return journalNo;
+	}
+
+	public void setJournalNo(Integer journalNo) {
+		this.journalNo = journalNo;
+	}
+
+	public String getProprietorName() {
+		return proprietorName;
+	}
+
+	public void setProprietorName(String proprietorName) {
+		this.proprietorName = proprietorName;
+	}
+
+	public String getProprietorAddress() {
+		return proprietorAddress;
+	}
+
+	public void setProprietorAddress(String proprietorAddress) {
+		this.proprietorAddress = proprietorAddress;
+	}
+
+}
