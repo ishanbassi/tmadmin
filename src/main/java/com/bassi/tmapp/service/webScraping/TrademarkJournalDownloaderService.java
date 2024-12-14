5a23b7be81fa65ba20bc6231fff34143ea5ba11c
ishanbassi <62026183+ishanbassi@users.noreply.github.com>
2024-12-14 16:35:13 +0530

    minor change

--- src/main/java/com/bassi/tmapp/service/webScraping/TrademarkJournalDownloaderService.java
+++ src/main/java/com/bassi/tmapp/service/webScraping/TrademarkJournalDownloaderService.java
@@ -1,11 +1,14 @@
 package com.bassi.tmapp.service.webScraping;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -52,6 +55,7 @@ public class TrademarkJournalDownloaderService {
     @EventListener(ApplicationReadyEvent.class)
 	void downloadPdf() {
         try {
+        	System.out.println(System.getProperty("javax.net.ssl.trustStore"));
 			Document doc = Jsoup.connect(trademarkJournalBaseURL + "Trademark").get();
 			Element journalElement = doc.getElementById("Journal");
 			// We will download only the first journal
@@ -64,24 +68,39 @@ public class TrademarkJournalDownloaderService {
 			log.info("Found {} pdf files to download", fileNameInput.size());
 			
 			Path journalDirectory  = Files.createDirectories(Paths.get(basePdfDirectory + "/" + journalNo ));
+			
 
 			
 			 for (Element input : fileNameInput) {
 				 	RestTemplate restTemplate = new RestTemplate();
 		            String pdfFileName = input.attr("value");
 		            String completeUrl = trademarkJournalBaseURL + "ViewJournal";
+		            
+		            // extract last part of the file name
+		            String sanitizedPdfFileName;
+		            String regex = "[^\\\\]+$"; 
+
+		            Pattern pattern = Pattern.compile(regex);
+		            Matcher matcher = pattern.matcher(pdfFileName);
+		            if (matcher.find()) {
+		            	sanitizedPdfFileName = matcher.group();
+		            } else {
+		            	sanitizedPdfFileName = "";
+		            }
 		            HttpHeaders headers = new HttpHeaders();
 		            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 		            MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
 		            formData.add("FileName", pdfFileName);
 
 		            
-		            log.info("Going to download {} using the url: {}", pdfFileName, completeUrl);
+		            log.info("Going to download {} using the url: {}", sanitizedPdfFileName, completeUrl);
 
 		            HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(formData,headers);
 		            ResponseEntity<byte[]> result  = restTemplate.postForEntity(completeUrl, entity,byte[].class);
 		            byte[] bytes = result.getBody();
-//		            FileUtils.writeByteArrayToFile(new File("pathname"), myByteArray);
+		            
+		            Files.write(new File(journalDirectory + "/" + sanitizedPdfFileName).toPath(), bytes);
+		            
 
 		            OutputStream out = new FileOutputStream(journalDirectory.toString());
 		            out.write(bytes);
